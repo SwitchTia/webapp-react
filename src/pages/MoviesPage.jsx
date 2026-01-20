0c32b433e67570970e94636ab84a40a3cd3ee415
SwitchTia <oblasser.tatiana@gmail.com>
2026-01-20 22:27:48 +0100

    Feat: rendering in page

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "./MoviesPage.css";
 
 
 export default function MoviesPage() {
@@ -7,6 +8,7 @@ export default function MoviesPage() {
 
   const [movies, setMovies] = useState([]);
 
+  
   useEffect(() => {
   
     axios
@@ -24,15 +26,37 @@ export default function MoviesPage() {
 
 
   return (
-    <>
-      <section >
-        
-        <ul>
-          {movies.map((movie) => (
-            <li key={movie.id}>{movie.title}</li>
-          ))}
-        </ul>
-      </section>
-    </>
+    <div className="movies-container">
+      <h1>All Movies</h1>
+      
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          
+          <div key={movie.id} className="movie-card">
+            
+            <div className="card-img">
+              <img 
+                src={`http://localhost:3000/imgs/${movie.image}`}
+                alt={movie.title}
+              />
+            </div>
+            
+            <div className="card-info">
+              <h3 className="card-title">{movie.title}</h3>
+              
+              <div className="card-details">
+                <p>Director: {movie.director}</p>
+                <p>Genre: {movie.genre}</p>
+                <p>Release year: {movie.year}</p>
+                <p className="overview">{movie.abstract}</p>
+              </div>
+            </div>
+            
+          </div>
+          
+        ))}
+      </div>
+      
+    </div>
   );
 }
