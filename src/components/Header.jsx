d1a45d85d35e7d7ec36ceb00b4785b0c54c273cb
SwitchTia <oblasser.tatiana@gmail.com>
2026-01-20 19:04:16 +0100

    Feat: header.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,24 +3,41 @@ import "./Header.css";
 
 
 export default function Header({ nameApp }) {
-  
-
-  const headerLinks = [
-    { title: "Home", path: "/" },
-    { title: "Movies", path: "/moovies" },
-  ];
 
 
-  return (
-   <header className="header">
+    const headerLinks = [
+
+        { title: "Home", path: "/" },
+        { title: "Movies", path: "/moovies" },
+
+    ];
+
+
+    return (
+        <header className="header">
             <div className="header-content">
                 <div className="logo">WEBAPP</div>
+                <div>
+                    <ul className="nav-item">
+                        {headerLinks.map((link, index) => (
+                            <li  key={index}>
+                                <NavLink
+                                    className="nav-link"
+                                    aria-current="page"
+                                    to={link.path}
+                                >
+                                    {link.title}
+                                </NavLink>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
 
                 <div className="search-bar">
                     <input
                         type="text"
                         placeholder="Searching for something?"
-                        
+
                     />
 
                     <button >Search</button>
@@ -28,5 +45,5 @@ export default function Header({ nameApp }) {
 
             </div>
         </header>
-  );
+    );
 }
