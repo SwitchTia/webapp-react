79eceb6128578b2436d9e14d335d6212026f41e2
SwitchTia <oblasser.tatiana@gmail.com>
2026-01-21 21:58:50 +0100

    Feat: reviewCard

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,14 @@
+export default function ReviewCard({ review }) {
+    
+  return (
+    <div className="review-card">
+      
+        <h4 className="card-title">
+          Written by {review.name} <span>{review.vote}</span>
+        </h4>
+        <p className="card-text">{review.text}</p>
+        <p className="text-end">{review.created_at}</p>
+  
+    </div>
+  );
+}
