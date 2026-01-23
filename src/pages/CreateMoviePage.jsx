38959d41011716a94ff1a4d1275be620e4e4d07b
SwitchTia <oblasser.tatiana@gmail.com>
2026-01-23 22:52:29 +0100

    Feat: createMoviePage wip

---
+++ src/pages/CreateMoviePage.jsx
@@ -0,0 +1,50 @@
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+
+const initialData = {
+  title: "",
+  author: "",
+  abstract: "",
+  image: null,
+};
+
+export default function CreateBookPage() {
+  const [formData, setFormData] = useState(initialData);
+  const [imagePreviewUrl, setImagePreviewUrl] = useState(null);
+
+  
+
+  const navigate = useNavigate();
+ 
+
+
+
+  function updateFormData(event) {
+    //to complete
+  }
+
+
+
+  function handleSubmitForm(event) {
+    event.preventDefault();
+    // to complete
+  }
+
+  return (
+    <section className="movie-form">
+      <h1 >Add a new movies</h1>
+    
+        <div >
+          <form >
+          
+
+            <button >Submit</button>
+          </form>
+        </div>
+    
+    </section>
+  );
+}
